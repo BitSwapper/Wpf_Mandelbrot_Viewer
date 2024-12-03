361162f0f7b7897b284b6bf033a848d121d0a7f9
BitSwapper <killthirster@gmail.com>
2024-12-03 13:32:40 -0500

    cleanup

--- MandelbrotWindow.xaml.cs
+++ MandelbrotWindow.xaml.cs
@@ -1,27 +1,24 @@
 ﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 
 namespace WpfMandelbrot;
 
 public partial class MandelbrotWindow : Window
 {
-    private MainWindow mainWindow;
-    private MandelbrotEffect mandelbrotEffect;
+ MainWindow mainWindow;
+ MandelbrotEffect mandelbrotEffect;
 
     public MandelbrotWindow(MainWindow main)
     {
         InitializeComponent();
         mainWindow = main;
 
-        // Initialize the effect
         mandelbrotEffect = new MandelbrotEffect();
         MandelbrotImage.Effect = mandelbrotEffect;
 
         this.Loaded += (s, e) => UpdateMandelbrot(0, 0, 1, new RenderSettings
         {
-            MaxIterations = 600,
+            MaxIterations = 1000,
             ColorMode = ColorMode.Rainbow,
             Quality = RenderQuality.Normal,
             SmoothColors = true
@@ -42,7 +39,7 @@ public partial class MandelbrotWindow : Window
         mandelbrotEffect.ColorMode = (double)settings.ColorMode;
     }
 
-    private void MandelbrotWindow_SizeChanged(object sender, SizeChangedEventArgs e)
+ void MandelbrotWindow_SizeChanged(object sender, SizeChangedEventArgs e)
     {
         if(MandelbrotImage.ActualWidth > 0 && MandelbrotImage.ActualHeight > 0)
         {
@@ -50,15 +47,13 @@ public partial class MandelbrotWindow : Window
         }
     }
 
-    private void MandelbrotImage_MouseDown(object sender, MouseButtonEventArgs e)
+ void MandelbrotImage_MouseDown(object sender, MouseButtonEventArgs e)
     {
         Point pos = e.GetPosition(MandelbrotImage);
 
-        // Convert to normalized coordinates (0 to 1)
         double normalizedX = pos.X / MandelbrotImage.ActualWidth;
         double normalizedY = pos.Y / MandelbrotImage.ActualHeight;
 
-        // Transform to match shader coordinates
         double minDim = Math.Min(MandelbrotImage.ActualWidth, MandelbrotImage.ActualHeight);
         double newX = (normalizedX - 0.5) * MandelbrotImage.ActualWidth / minDim / (mainWindow.Zoom * 0.5) + mainWindow.CenterX;
         double newY = (normalizedY - 0.5) * MandelbrotImage.ActualHeight / minDim / (mainWindow.Zoom * 0.5) + mainWindow.CenterY;
